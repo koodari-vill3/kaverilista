62b58d42c5b791d057a0e0e6362e32c0801083d5
koodari-vill3 <58549590+koodari-vill3@users.noreply.github.com>
2020-08-28 08:08:53 +0300

    Add files via upload

---
+++ array.js
@@ -0,0 +1,15 @@
+let name = 'Simo';
+let name2 = 'Sami';
+
+let names = new array ('Simo','Sami','Jeppe')
+let fruits = ['apples','oranges','pears']
+
+names.push('Maikki');
+names.push('Mustikki');
+
+let removedItem = names.pop();
+document.write(`Poistettiin ${removedItem} listasta`);
+
+removedItems = names.shift();
+document.write(`Poistettiin ${removedItem} listasta`);
+names.unshift('Keijo');
