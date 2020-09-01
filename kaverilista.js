8adbb54bb47999e5778471a3d6a8afc98ab45b9d
koodari-vill3 <58549590+koodari-vill3@users.noreply.github.com>
2020-09-01 09:53:31 +0300

    Update kaverilista.js

--- kaverilista.js
+++ kaverilista.js
@@ -1,10 +1,20 @@
 let list = [];
 
-function addItem(){
+function addItem(item){
     list.push(item);
+    printlist();
 
 }
 
 function printlist(){
+    let liElems = '';
+    for(let i=0; i<list.length; i++){
+       liElems = liElems + `<li>${list[i]}</li>`;
+    }
+    document.getElementById('Kaverilista').innerHTML = liElems;
+}
 
+function removeItem(){
+    list.pop();
+    printlist();
 }
